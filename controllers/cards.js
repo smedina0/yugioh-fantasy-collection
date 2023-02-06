017497ef28e042aa92742101341f2e6061e739ab
Steven Medina <smedina2494@gmail.com>
2023-02-05 19:19:57 -0500

    Feat: Adds update route and fixes the new/edit forms

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,166 +13,195 @@ const cloudinary = require("cloudinary").v2;
 // Seed Route
 
 router.get('/yugioh/seed', (req, res) => {
-    Card.deleteMany({}, (err, results)=>{
-    Card.create(data, (err, results)=>{
-    res.redirect('/yugioh');
-    });
-    });
-    });
-    
-    // Loop through json for each, build a new object with the right fields
-    
-    var pagination = require('pagination');
-    var paginator = pagination.create('search', {prelink:'/', current: 1, rowsPerPage: 200, totalResult: 10020});
-
-    // * Mount Routes
-    
-    // Index
-    router.get("/yugioh", (req, res) => {
-        // Card.find({}, (error, allCards) => {
-        //     res.render("index.ejs", {
-        //         cards: allCards,
-        //         cardData: cardData,
-        //     });
-        // });
-
-const pageOptions = {
-    page: parseInt(req.query.page, 10) || 0,
-    limit: parseInt(req.query.limit, 10) || 10
-}
-
-        Card.find({}).skip(10).limit(100).exec(function(err, results) {
-            res.render("index.ejs", {
-                        cards: allCards,
-                        cardData: cardData,
-                    });
+    Card.deleteMany({}, (err, results) => {
+        Card.create(data, (err, results) => {
+            res.redirect('/yugioh');
         });
     });
+});
+
+// Loop through json for each, build a new object with the right fields
+
+var pagination = require('pagination');
+var paginator = pagination.create('search', {
+    prelink: '/',
+    current: 1,
+    rowsPerPage: 200,
+    totalResult: 10020
+});
+
+// * Mount Routes
+
+// Index
+router.get("/yugioh", (req, res) => {
+    // Card.find({}, (error, allCards) => {
+    //     res.render("index.ejs", {
+    //         cards: allCards,
+    //         cardData: cardData,
+    //     });
+    // });
+
+    const pageOptions = {
+        page: parseInt(req.query.page, 10) || 0,
+        limit: parseInt(req.query.limit, 10) || 10
+    }
+
+    Card.find({}).skip(10).limit(100).exec(function (err, results) {
+        res.render("index.ejs", {
+            cards: allCards,
+            cardData: cardData,
+        });
+    });
+});
 
 
 
 
-    // - User List Index
+// - User List Index
 
-    router.get("/yugioh/mycards", (req, res) => {
-        UserCard.find({userId: req.session.userId}, (error, userCards) => {
-            Card.find({_id: userCards.map(userCard => userCard.cardId)}, (error, allCards) => {
-                res.render("userCards/index.ejs", {
-                    cards: allCards,
-                    cardData: cardData
-                });
+router.get("/yugioh/mycards", (req, res) => {
+    UserCard.find({
+        userId: req.session.userId
+    }, (error, userCards) => {
+        Card.find({
+            _id: userCards.map(userCard => userCard.cardId)
+        }, (error, allCards) => {
+            res.render("userCards/index.ejs", {
+                cards: allCards,
+                cardData: cardData
             });
-        })
+        });
+    })
 
-    });
-
-
-    // New
-    
-    router.get("/yugioh/new", (req, res) => {
-        res.render("new.ejs");
-    });
+});
 
 
-    
-    // Delete
-    
+// New
+
+router.get("/yugioh/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+
+
+// Delete
+
 
 router.delete("/yugioh/mycards/:id", (req, res) => {
     Card.findByIdAndRemove(req.params.id, (err, data) => {
         res.redirect("/yugioh/mycards");
     })
-  })
-    
-    // Update
-    
-    
-    // Create
-   
+})
 
-    router.post("/yugioh", (req, res) => {
-        const src = req.files.src;
-        src.mv(`./uploads/${src.name}`);
-        
-      
-     
-        cloudinary.uploader.upload(`./uploads/${src.name}`, (err, result) => {
-            req.body.src = result.secure_url;
-            console.log(err, result);
-            Card.create(req.body, (error, createdCard) => {
-                res.redirect("/yugioh");
+// Update
+
+router.put("/yugioh/mycards/:id", (req, res) => {
+    
+    Card.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+        },
+        (error, updatedCard) => {
+          res.redirect(`/yugioh/mycards/${req.params.id}`)
         });
+  });
 
-      
-        });
-    });
 
-    router.post("/yugioh", (req, res) => {
+// Create
 
-       
+
+router.post("/yugioh", (req, res) => {
+    const src = req.files.src;
+    src.mv(`./uploads/${src.name}`);
+
+
+
+    cloudinary.uploader.upload(`./uploads/${src.name}`, (err, result) => {
+        req.body.src = result.secure_url;
+        // console.log(err, result);
         Card.create(req.body, (error, createdCard) => {
-           
             res.redirect("/yugioh");
         });
-    });
 
-    router.post("/yugioh/mycards", (req, res) => {
-        
-        UserCard.create(req.body, (error, createdCard) => {
-            res.redirect("/yugioh/mycards");
+
+    });
+});
+
+router.post("/yugioh", (req, res) => {
+
+
+    Card.create(req.body, (error, createdCard) => {
+
+        res.redirect("/yugioh");
+    });
+});
+
+router.post("/yugioh/mycards", (req, res) => {
+
+    UserCard.create(req.body, (error, createdCard) => {
+        res.redirect("/yugioh/mycards");
+    });
+});
+
+
+// router.get("/yugioh-createCards", (req, res) => {
+//     cardData.forEach(function(card) {
+//         // console.log(card.card_sets)
+//         if (card.card_sets == undefined) { return }
+//         if (card.card_sets[0] == undefined) { return }
+//         Card.create({
+//           name: card.name,
+//           boxSet: card.card_sets[0].set_name,
+//         cardDescription: card.desc,
+//         img: card.card_images[0].image_url,
+//         cardType: card.type,
+//         price: card.card_prices[0].tcgplayer_price,
+//         attack: card.atk,
+//         defense: card.def,
+//         race: card.race,
+//         archetype: card.archetype,
+//         rarity: card.card_sets[0].set_rarity,
+//         attribute: card.attribute,
+//         level: card.level
+
+//         })
+
+//       })
+//       res.redirect("/yugioh");
+// })
+
+
+
+// Edit
+
+router.get("/yugioh/mycards/:id/edit", (req, res) => {
+    Card.findById(req.params.id, (error, foundCard) => {
+      res.render("edit.ejs", {
+        card: foundCard,
+      })
+    })
+  })
+
+//Show
+
+router.get("/yugioh/:id", (req, res) => {
+    Card.findById(req.params.id, (err, foundCard) => {
+        res.render("show.ejs", {
+            card: foundCard,
         });
     });
-  
-    
-    // router.get("/yugioh-createCards", (req, res) => {
-    //     cardData.forEach(function(card) {
-    //         // console.log(card.card_sets)
-    //         if (card.card_sets == undefined) { return }
-    //         if (card.card_sets[0] == undefined) { return }
-    //         Card.create({
-    //           name: card.name,
-    //           boxSet: card.card_sets[0].set_name,
-    //         cardDescription: card.desc,
-    //         img: card.card_images[0].image_url,
-    //         cardType: card.type,
-    //         price: card.card_prices[0].tcgplayer_price,
-    //         attack: card.atk,
-    //         defense: card.def,
-    //         race: card.race,
-    //         archetype: card.archetype,
-    //         rarity: card.card_sets[0].set_rarity,
-    //         attribute: card.attribute,
-    //         level: card.level
-    
-    //         })
-           
-    //       })
-    //       res.redirect("/yugioh");
-    // })
-    
+});
 
-    
-    // Edit
-    
-    
-    //Show
-    
-    router.get("/yugioh/:id", (req, res) => {
-        Card.findById(req.params.id, (err, foundCard) => {
-            res.render("show.ejs", {
-                card: foundCard,
-            });
+// - User List Show
+
+router.get("/yugioh/mycards/:id", (req, res) => {
+    Card.findById(req.params.id, (err, foundCard) => {
+        res.render("userCards/show.ejs", {
+            card: foundCard,
         });
     });
-
-     // - User List Show
-
-     router.get("/yugioh/mycards/:id", (req, res) => {
-        Card.findById(req.params.id, (err, foundCard) => {
-            res.render("userCards/show.ejs", {
-                card: foundCard,
-            });
-        });
-    });
+});
 
 module.exports = router;
