70c7809ebebef4636d6ac02b13b1a177125e1d0e
Steven Medina <smedina2494@gmail.com>
2023-04-09 03:14:37 -0400

    Feat: Adds pagination to the project

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,128 +2,175 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data');
 const Card = require('../models/card');
-const UserCard = require("../models/userCard");
-const fs = require("fs");
-let rawData = fs.readFileSync("./models/card_info.json");
+const UserCard = require('../models/userCard');
+const fs = require('fs');
+let rawData = fs.readFileSync('./models/card_info.json');
 const cardData = JSON.parse(rawData).data;
-const cloudinary = require("cloudinary").v2;
-
-
+const cloudinary = require('cloudinary').v2;
 
 // Seed Route
 
 router.get('/yugioh/seed', (req, res) => {
-    Card.deleteMany({}, (err, results) => {
-        Card.create(data, (err, results) => {
-            res.redirect('/yugioh');
-        });
+  Card.deleteMany({}, (err, results) => {
+    Card.create(data, (err, results) => {
+      res.redirect('/yugioh');
     });
+  });
 });
 
 // * Mount Routes
 
+// // Index
+// router.get("/yugioh", (req, res) => {
+//     Card.find({}, (error, allCards) => {
+//         res.render("index.ejs", {
+//             cards: allCards,
+//             cardData: cardData,
+//         });
+//     })
+// });
+
 // Index
-router.get("/yugioh", (req, res) => {
-    Card.find({}, (error, allCards) => {
-        res.render("index.ejs", {
-            cards: allCards,
-            cardData: cardData,
+router.get('/yugioh', (req, res) => {
+  const currentLetter = req.query.letter || 'A';
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  User.findById(req.session.userId, (err, foundUser) => {
+    Card.find(
+      { name: new RegExp('^' + currentLetter, 'i') },
+      (error, allCards) => {
+        if (error) {
+          console.log(error);
+          return res.redirect('/yugioh');
+        }
+
+        res.render('index.ejs', {
+          cards: allCards,
+          cardData: cardData,
+          currentLetter: currentLetter,
+          alphabet: alphabet,
+          user: foundUser, // pass the user object to the view
         });
-    })
+      }
+    );
+  });
 });
-
-
-
 
 // - User List Index
 
-router.get("/yugioh/mycards", (req, res) => {
-    UserCard.find({
-        userId: req.session.userId
-    }, (error, userCards) => {
-        Card.find({
-            _id: userCards.map(userCard => userCard.cardId)
-        }, (error, allCards) => {
-            res.render("userCards/index.ejs", {
-                cards: allCards,
-                cardData: cardData
-            });
-        });
-    })
-
+router.get('/yugioh/mycards', (req, res) => {
+  UserCard.find(
+    {
+      userId: req.session.userId,
+    },
+    (error, userCards) => {
+      Card.find(
+        {
+          _id: userCards.map((userCard) => userCard.cardId),
+        },
+        (error, allCards) => {
+          res.render('userCards/index.ejs', {
+            cards: allCards,
+            cardData: cardData,
+          });
+        }
+      );
+    }
+  );
 });
-
 
 // New
 
-router.get("/yugioh/new", (req, res) => {
-    res.render("new.ejs");
+router.get('/yugioh/new', (req, res) => {
+  res.render('new.ejs');
 });
-
-
 
 // Delete
 
-
-router.delete("/yugioh/mycards/:id", (req, res) => {
-    Card.findByIdAndRemove(req.params.id, (err, data) => {
-        res.redirect("/yugioh/mycards");
-    })
-})
+router.delete('/yugioh/mycards/:id', (req, res) => {
+  Card.findByIdAndRemove(req.params.id, (err, data) => {
+    res.redirect('/yugioh/mycards');
+  });
+});
 
 // Update
 
-router.put("/yugioh/mycards/:id", (req, res) => {
-    
-    Card.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {
-          new: true,
-        },
-        (error, updatedCard) => {
-          res.redirect(`/yugioh/mycards/${req.params.id}`)
-        });
-  });
-
+router.put('/yugioh/mycards/:id', (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+    (error, updatedCard) => {
+      res.redirect(`/yugioh/mycards/${req.params.id}`);
+    }
+  );
+});
 
 // Create
 
+router.post('/yugioh', (req, res) => {
+  Card.findOne({ name: req.body.name }, (error, foundCard) => {
+    if (error) {
+      console.log(error);
+      return res.redirect('/yugioh');
+    }
+    if (foundCard) {
+      UserCard.create(
+        { userId: req.session.userId, cardId: foundCard._id },
+        (error, createdUserCard) => {
+          if (error) {
+            console.log(error);
+            return res.redirect('/yugioh');
+          }
+          res.redirect('/yugioh/mycards');
+        }
+      );
+    } else {
+      const src = req.files.src;
+      src.mv(`./uploads/${src.name}`);
 
-router.post("/yugioh", (req, res) => {
-    const src = req.files.src;
-    src.mv(`./uploads/${src.name}`);
-
-
-
-    cloudinary.uploader.upload(`./uploads/${src.name}`, (err, result) => {
+      cloudinary.uploader.upload(`./uploads/${src.name}`, (err, result) => {
         req.body.src = result.secure_url;
         console.log(err, result);
         Card.create(req.body, (error, createdCard) => {
-            UserCard.create(req.body, (error, createdCard) => {
-            res.redirect("/yugioh");
+          if (error) {
+            console.log(error);
+            return res.redirect('/yugioh');
+          }
+          UserCard.create(
+            { userId: req.session.userId, cardId: createdCard._id },
+            (error, createdUserCard) => {
+              if (error) {
+                console.log(error);
+                return res.redirect('/yugioh');
+              }
+              res.redirect('/yugioh/mycards');
+            }
+          );
         });
-    });
-
-    });
+      });
+    }
+  });
 });
 
-router.post("/yugioh", (req, res) => {
+// Add to User Cards
+router.post('/yugioh/mycards', (req, res) => {
+  const userId = req.session.userId;
+  const cardId = req.body.cardId;
 
-
-    Card.create(req.body, (error, createdCard) => {
-        res.redirect("/yugioh");
-    });
-
+  UserCard.create(
+    { userId: userId, cardId: cardId },
+    (error, createdUserCard) => {
+      if (error) {
+        console.log(error);
+        return res.redirect('/yugioh');
+      }
+      res.redirect('/yugioh/mycards');
+    }
+  );
 });
-
-router.post("/yugioh/mycards", (req, res) => {
-
-    UserCard.create(req.body, (error, createdCard) => {
-        res.redirect("/yugioh/mycards");
-    });
-});
-
 
 // router.get("/yugioh-createCards", (req, res) => {
 //     cardData.forEach(function(card) {
@@ -151,36 +198,34 @@ router.post("/yugioh/mycards", (req, res) => {
 //       res.redirect("/yugioh");
 // })
 
-
-
 // Edit
 
-router.get("/yugioh/mycards/:id/edit", (req, res) => {
-    Card.findById(req.params.id, (error, foundCard) => {
-      res.render("edit.ejs", {
-        card: foundCard,
-      })
-    })
-  })
+router.get('/yugioh/mycards/:id/edit', (req, res) => {
+  Card.findById(req.params.id, (error, foundCard) => {
+    res.render('edit.ejs', {
+      card: foundCard,
+    });
+  });
+});
 
 //Show
 
-router.get("/yugioh/:id", (req, res) => {
-    Card.findById(req.params.id, (err, foundCard) => {
-        res.render("show.ejs", {
-            card: foundCard,
-        });
+router.get('/yugioh/:id', (req, res) => {
+  Card.findById(req.params.id, (err, foundCard) => {
+    res.render('show.ejs', {
+      card: foundCard,
     });
+  });
 });
 
 // - User List Show
 
-router.get("/yugioh/mycards/:id", (req, res) => {
-    Card.findById(req.params.id, (err, foundCard) => {
-        res.render("userCards/show.ejs", {
-            card: foundCard,
-        });
+router.get('/yugioh/mycards/:id', (req, res) => {
+  Card.findById(req.params.id, (err, foundCard) => {
+    res.render('userCards/show.ejs', {
+      card: foundCard,
     });
+  });
 });
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -1,98 +1,116 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 const Card = require('../models/card');
-const fs = require("fs");
-let rawData = fs.readFileSync("./models/card_info.json");
+const fs = require('fs');
+let rawData = fs.readFileSync('./models/card_info.json');
 const cardData = JSON.parse(rawData).data;
-const pagination = require("pagination");
+const pagination = require('pagination');
 const paginator = pagination.create('search', {
-    prelink: '/yugioh',
-    current: 1,
-    rowsPerPage: 200,
-    totalResult: 10020
+  prelink: '/yugioh',
+  current: 1,
+  rowsPerPage: 200,
+  totalResult: 10020,
 });
 
+router.get('/yugioh', (req, res) => {
+  const currentLetter = req.query.letter || 'A';
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
-// Index
-router.get("/yugioh", (req, res) => {
-    Card.find({}, (error, allCards) => {
-        res.render("index.ejs", {
-            cards: allCards,
-            cardData: cardData,
+  User.findById(req.session.userId, (err, foundUser) => {
+    Card.find(
+      { name: new RegExp('^' + currentLetter, 'i') },
+      (error, allCards) => {
+        if (error) {
+          console.log(error);
+          return res.redirect('/yugioh');
+        }
+
+        res.render('index.ejs', {
+          cards: allCards,
+          cardData: cardData,
+          currentLetter: currentLetter,
+          alphabet: alphabet,
+          user: foundUser, // pass the user object to the view
         });
-    });
-
+      }
+    );
+  });
 });
 
 // Sign up
 
 router.get('/signup', (req, res) => {
-    res.render("signup.ejs", {
-        error: null
-    });
+  res.render('signup.ejs', {
+    error: null,
+  });
 });
 
 // Handle form Submission
 
 router.post('/signup', (req, res) => {
-    let error = null;
-    if (req.body.password !== req.body.passwordConf) {
-        error = "Password and password confirmation do not match.";
-        return res.render("signup.ejs", {
-            error
-        });
-    }
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    req.body.password = hashedPassword;
-    User.create(req.body, (err, newUser) => {
-        req.session.userId = newUser._id;
-        res.redirect("/yugioh");
-    })
+  let error = null;
+  if (req.body.password !== req.body.passwordConf) {
+    error = 'Password and password confirmation do not match.';
+    return res.render('signup.ejs', {
+      error,
+    });
+  }
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+  User.create(req.body, (err, newUser) => {
+    req.session.userId = newUser._id;
+    res.redirect('/yugioh');
+  });
 });
 
 // Login
 
-router.get("/login", (req, res) => {
-    res.render("login.ejs", {
-        error: null
-    });
+router.get('/login', (req, res) => {
+  res.render('login.ejs', {
+    error: null,
+  });
 });
 
-router.post("/login", (req, res) => {
-    // Look up user
-    const error = "Incorrect Credentials.";
-    User.findOne({
-        email: req.body.email
-    }, (err, foundUser) => {
-        if (!foundUser) {
-            return res.render("login.ejs", {
-                error
-            });
-        }
+router.post('/login', (req, res) => {
+  // Look up user
+  const error = 'Incorrect Credentials.';
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, foundUser) => {
+      if (!foundUser) {
+        return res.render('login.ejs', {
+          error,
+        });
+      }
 
-        const isMatched = bcrypt.compareSync(req.body.password, foundUser.password);
+      const isMatched = bcrypt.compareSync(
+        req.body.password,
+        foundUser.password
+      );
 
-        if (!isMatched) {
-            return res.render("login.ejs", {
-                error
-            });
-        };
-        // creates a session for the authenticated user
-        req.session.userId = foundUser._id;
+      if (!isMatched) {
+        return res.render('login.ejs', {
+          error,
+        });
+      }
+      // creates a session for the authenticated user
+      req.session.userId = foundUser._id;
 
-        res.redirect("/yugioh")
-    });
+      res.redirect('/yugioh');
+    }
+  );
 });
 
 // Logout
 
-router.get("/logout", (req, res) => {
-    req.session.destroy((err) => {
-        res.redirect("login.ejs");
-    });
-
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect('login.ejs');
+  });
 });
 
 module.exports = router;
